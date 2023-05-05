5a01d1d55d4e51a8230af15d6efc28d521a74e95
New Supagrit <supagrit5@gmail.com>
2023-05-05 13:37:10 +0700

    sidebarnav

--- resources/js/Components/MenuItem.vue
+++ resources/js/Components/MenuItem.vue
@@ -7,19 +7,19 @@ const props = defineProps({ iconString: String });
 const { iconString } = toRefs(props);
 
 import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
-import Menu from "vue-material-design-icons/Menu.vue";
 import BellOutline from "vue-material-design-icons/BellOutline.vue";
 import Magnify from "vue-material-design-icons/Magnify.vue";
+import CogOutline from "vue-material-design-icons/CogOutline.vue";
 let icon = null;
 if (iconString.value === "Home") icon = HomeOutline;
-if (iconString.value === "Logout") icon = Menu;
+if (iconString.value === "Logout") icon = CogOutline;
 if (iconString.value === "Notification") icon = BellOutline;
 if (iconString.value === "Search") icon = Magnify;
 </script>
 
 <template>
     <div class="w-full">
-        <div class="flex items-center">
+        <div class="flex items-center px-5 py-2 rounded-2xl hover:bg-gray-100">
             <div v-if="iconString === 'Profile'" class="mr-1">
                 <img
                     class="rounded-full ml-[2px] w-[32px] cursor-pointer border-[2px] border-black"
@@ -30,12 +30,10 @@ if (iconString.value === "Search") icon = Magnify;
                 v-else
                 :is="icon"
                 fillColor="#000000"
-                :size="36"
+                :size="34"
                 class="pr-[1px]"
             ></component>
-            <span
-                class="text-[18px] xl:block hidden font-bold text-black pl-3 pt-1"
-            >
+            <span class="text-[18px] font-bold text-black pl-3 pt-1">
                 {{ iconString }}
             </span>
         </div>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,22 @@
 <script setup>
 import MenuItem from "@/Components/MenuItem.vue";
+
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+let showSideBar = ref(false);
+let showSearchBar = ref(false);
+
+function triggerSideBar() {
+    showSideBar.value = !showSideBar.value;
+}
+function triggerSearchBar() {
+    showSearchBar.value = !showSearchBar.value;
+}
+
+import Menu from "vue-material-design-icons/Menu.vue";
+import Close from "vue-material-design-icons/Close.vue";
+import Magnify from "vue-material-design-icons/Magnify.vue";
 </script>
 
 <template>
@@ -8,33 +24,74 @@ import { Link } from "@inertiajs/vue3";
         <nav
             v-if="$page.url === '/'"
             id="TopNav"
-            class="absolute w-full bg-white h-[61px] border-b border-t border-gray-400 z-0"
-        ></nav>
+            class="fixed flex items-center justify-between lg:justify-end w-full bg-white h-[61px] border-b border-t border-gray-400 z-5"
+        >
+            <div
+                class="lg:hidden flex items-center justify-between w-[100px] py-auto"
+            >
+                <button
+                    @click="triggerSideBar"
+                    type="button"
+                    class="hover:bg-gray-100 mx-5"
+                >
+                    <Menu
+                        fillColor="#808080"
+                        :size="36"
+                        class="block pl-2 rounded-2xl"
+                    ></Menu>
+                </button>
+                <Link href="/">
+                    <p class="">HOME</p>
+                </Link>
+            </div>
+            <div class="flex items-center justify-between flex-row mr-10">
+                <Magnify fillColor="#808080" :size="30"></Magnify>
+                <input
+                    id="Search"
+                    type="text"
+                    class="py-0 px-2 ml-2 rounded-3xl lg:w-[400px] w-[120px]"
+                    placeholder="Search"
+                />
+            </div>
+        </nav>
         <nav
             id="SideBar"
-            class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-400 z-2 shadow-2xl"
+            class="fixed h-full bg-white w-[280px] lg:transform-none border-r border-r-gray-400 z-10 shadow-2xl transform-gpu transition-all"
+            :class="showSideBar ? 'translate-x' : '-translate-x-full'"
         >
             <Link href="/">
-                <p class="xl:hidden block">home</p>
-                <p class="xl:block hidden">HOME</p>
+                <p>HOME</p>
             </Link>
+            <button
+                @click="triggerSideBar"
+                type="button"
+                class="absolute top-0 right-0 m-3 lg:hidden hover:bg-gray-100"
+            >
+                <Close
+                    fillColor="#808080"
+                    :size="36"
+                    class="block px-2 rounded-2xl"
+                ></Close>
+            </button>
             <div class="px-5 pt-10">
-                <MenuItem iconString="Home" class="mb-7"></MenuItem>
-                <MenuItem iconString="Search" class="mb-7"></MenuItem>
-                <MenuItem iconString="Notification" class="mb-7"></MenuItem>
-                <MenuItem iconString="Profile" class="mb-7"></MenuItem>
+                <Link href="/"
+                    ><MenuItem iconString="Home" class="mb-3"></MenuItem>
+                </Link>
+                <MenuItem iconString="Search" class="mb-3"></MenuItem>
+                <MenuItem iconString="Notification" class="mb-3"></MenuItem>
+                <MenuItem iconString="Profile" class="mb-3"></MenuItem>
             </div>
             <Link href="/" class="absolute bottom-0 px-5 w-full">
-                <MenuItem iconString="Logout" class="mb-7"></MenuItem>
+                <MenuItem iconString="Logout" class="mb-3"></MenuItem>
             </Link>
         </nav>
         <div
-            class="flex lg:justify-between bg-gray-100 h-full w-[100%-280px] xl:pl-[280px] md:pl-[100px] overflow-auto pt-[61px]"
+            class="flex lg:justify-between bg-gray-100 h-full w-[100%-280px] lg:pl-[280px] overflow-auto pt-[61px]"
         >
             <div
-                class="mx-auto md:pt-7 pt-20"
+                class="mx-auto lg:pt-7 pt-5"
                 :class="
-                    $page.url === '/' ? 'max-w-2xl w-full' : 'max-w-[1200px]'
+                    $page.url === '/' ? 'max-w-2xl    w-full' : 'max-w-[1200px]'
                 "
             >
                 <main>
