337cfb43b50145e36557f1e657d3c02e9ba4693d
New Supagrit <supagrit5@gmail.com>
2023-05-09 13:08:38 +0700

    homecontroller + postdata

---
+++ app/Http/Resources/AllPostsData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AllPostsData extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): object
+    {
+        return $this->PostData->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'text' => $post->text,
+                'file' => $post->file,
+                'create_at' => $post->create_at->format(' D M Y'),
+                'comments' => $post->comments->map(function ($comment) {
+                    return [
+                        'id' => $comment->id,
+                        'text' => $comment->text,
+                        'user' => [
+                            'id' => $comment->user->id,
+                            'text' => $comment->user->text,
+                            'file' => $comment->user->file,
+                        ]
+                    ];
+                }),
+                'likes' => $post->likes->map(function ($like) {
+                    return [
+                        'id' => $like->id,
+                        'user_id' => $like->user_idt,
+                        'post_id' => $like->post_id,
+                    ];
+                }),
+                'user' => [
+                    'id' => $post->user->id,
+                    'text' => $post->user->text,
+                    'file' => $post->user->file,
+                ]
+            ];
+        });
+    }
+}
